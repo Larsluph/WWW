92d6df97d4213a2d7c9b4bbedf18d0f6558ac746
Larsluph <remarso59@sfr.fr>
2021-10-16 11:24:51 +0200

    color fix

--- src/colors.h
+++ src/colors.h
@@ -9,9 +9,9 @@ static Color RED    { 255,   0,   0 };
 static Color GREEN  {   0, 255,   0 };
 static Color BLUE   {   0,   0, 255 };
 
-static Color YELLOW  =   RED | BLUE;
+static Color YELLOW  =   RED | GREEN;
 static Color CYAN    = GREEN | BLUE;
-static Color MAGENTA =   RED | GREEN;
+static Color MAGENTA =   RED | BLUE;
 static Color ORANGE { 255, 165,   0 };
 
 static Color WHITE = RED | GREEN | BLUE;
